a1883de16f0fdbf5dcd29fb01f36bd618b5c1061
iampratyancha <128408691+iampratyancha@users.noreply.github.com>
2023-03-26 14:33:51 +0530

    Create Area of reactangle.Iocal.java

---
+++ Area of reactangle.Iocal.java
@@ -0,0 +1,11 @@
+public class AOR_local {
+    public static void main (String args[])
+    {
+        int length = 5;
+        int breadth = 7;
+        double area;
+        area = length*breath;
+        System.out.println("Area of Reactangle\n" +area);
+
+    }
+}

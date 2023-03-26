e60441f1753aa56af7fd66635d175225a55c32cb
iampratyancha <128408691+iampratyancha@users.noreply.github.com>
2023-03-26 14:16:22 +0530

    Delete SimpleInterest.java

--- SimpleInterest.java
+++
@@ -1,8 +0,0 @@
-class SimpleInterest{
-    int p=32,r=5,t=2;
-    public static void main(String args[]){
-        SimpleInterest obj = new SimpleInterest();
-        float SI = (float)(obj.p*obj.r*obj.t)/100;
-        System.out.println("The simple interest calculated is: "+ SI);
-    }
-}

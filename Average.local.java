0b4360d8f3e13bc76e9e9062c88afe9080a2a95e
iampratyancha <128408691+iampratyancha@users.noreply.github.com>
2023-03-26 14:21:28 +0530

    Create Average.local.java

---
+++ Average.local.java
@@ -0,0 +1,11 @@
+public class Average_local {
+    public static void main(String args[])
+    {
+        int a = 23, b = 54, c = 56;
+        double Avg;
+        Avg = (a+b+c)/3;
+        System.out.println(Avg);
+
+    }
+    
+}

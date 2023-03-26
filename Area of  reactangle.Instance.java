ff4953e641a3c02a5bd31d3b053d5f1178d65bdf
iampratyancha <128408691+iampratyancha@users.noreply.github.com>
2023-03-26 14:32:11 +0530

    Create Area of  reactangle.Instance.java

---
+++ Area of  reactangle.Instance.java
@@ -0,0 +1,18 @@
+class AOR_Instance {
+int length;
+int breadth;
+double area;
+public void calculate_area()
+{
+    area = length*breath;
+    System.out.println("Area of Reactangle\n" +area);
+}
+
+    public static void main(String args[])
+    {
+        AOR_Instance A1 = new AOR_Instance();
+        A1.length=6;
+        A1.breadth=8;
+        A1.calculate_area();
+    }
+}

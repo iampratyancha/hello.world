89d1f1d4a8bceb49fa72890e3ddcea372fd3735d
iampratyancha <128408691+iampratyancha@users.noreply.github.com>
2023-03-26 14:23:59 +0530

    Create SI.local.java

---
+++ SI.local.java
@@ -0,0 +1,11 @@
+public class SI_local {
+    public static void main(String args[])
+    {
+        double p=500.00;
+        double r=2.5;
+        int t=3;
+        double SI;
+        SI = (p*r*t)/100;
+        System.out.println("Simple Intrest\n" +SI);
+    }
+}

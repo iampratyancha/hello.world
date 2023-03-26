70f66c0cc41bc561e0413a6b46fa99858be307d5
iampratyancha <128408691+iampratyancha@users.noreply.github.com>
2023-03-26 14:29:11 +0530

    Create Area of Circle.local.java

---
+++ Area of Circle.local.java
@@ -0,0 +1,12 @@
+public class AOC_local {
+    public static void main (String args[])
+    {
+        double Radius=6.2;
+        double area;
+         area = 3.14*Radius*Radius;
+        System.out.println("Area of circle is\n" +area);
+       
+
+    }
+    
+}

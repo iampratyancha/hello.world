b4a333ec22cae57f651dfd8489bdc87d056e3e69
iampratyancha <128408691+iampratyancha@users.noreply.github.com>
2023-03-26 14:16:13 +0530

    Delete InstancePythagoras.java

--- InstancePythagoras.java
+++
@@ -1,9 +0,0 @@
-class InstancePythagoras{
-    double adj=19,opp=7;
-    public static void main(String main[]){
-        InstancePythagoras obj = new InstancePythagoras();
-        double hypo1 = (Math.pow(obj.adj,2))+(Math.pow(obj.opp,2));
-        hypo1 = Math.sqrt(hypo1);
-        System.out.println("Hypotenuse: "+ hypo1);
-    }
-}
